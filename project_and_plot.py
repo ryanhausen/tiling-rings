5bf9a440c6bf638bdfe7beac94a0e6691345fe07
ryanhausen <rhausen@ucsc.edu>
2022-10-04 17:06:05 -0400

    points on sphere, wip stereographic project

--- project_and_plot.py
+++ project_and_plot.py
@@ -3,22 +3,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import axes3d
 
+from rings import build_rings
+
+d2r = np.pi / 180
+
 def main():
-    fig = plt.figure()
+    fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(projection='3d')
 
-    u = np.linspace(0, 2 * np.pi, 100)
-    v = np.linspace(0, np.pi, 100)
+    u, v = get_uv()
 
-    x = 10 * np.outer(np.cos(u), np.sin(v))
-    y = 10 * np.outer(np.sin(u), np.sin(v))
-    z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
+    x = np.cos(u) * np.sin(v)
+    y = np.sin(u) * np.sin(v)
+    z = np.cos(v)
 
-    ax.plot_wireframe(x, y, z)
+    ax.scatter(x, y ,z, color="b", marker=".")
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.set_zlim(-1, 1.2)
     ax.set_aspect("equal")
 
     plt.show()
 
+
+def get_uv_from_file():
+
+    with open("rings-c.txt", "r") as f:
+        lines = list(filter(
+            lambda x: len(x) == 3,
+            [list(map(float,l.strip().split())) for l in f.readlines()]
+        ))
+
+    v, u = zip(*list(map(lambda x: (x[1], x[2]), lines)))
+
+    return np.array(u), np.array(v)
+
+def get_uv(semicircle=True):
+    ring_data = build_rings()
+    u, v = [], []
+
+    for v_key in ring_data:
+        for u_data in ring_data[v_key]:
+            u.append(u_data * d2r)
+            v.append(v_key * d2r)
+
+    u, v = np.array(u), np.array(v) - np.pi / 2
+
+    if not semicircle:
+        non_equator = v!=(np.pi / 2)
+        flipped_v = v[non_equator] + np.pi
+        flipped_u = u[non_equator]
+
+        v = np.concatenate([v, flipped_v])
+        u = np.concatenate([u, flipped_u])
+
+    return u, v
 
 if __name__=="__main__":
     main()
--- rings.py
+++ rings.py
@@ -1,7 +1,38 @@
 
+from collections import OrderedDict
 import numpy as np
 
-def main():
+def build_rings(a_deg=3.955):
+    d2r = np.pi / 180
+
+    half_a = a_deg / 2 * d2r # half_a in radians
+    half_a_theta = np.arctan(half_a)
+
+    ring_data = OrderedDict()
+
+    d = 0
+    ring = 0
+
+    while d < np.pi / 2 - half_a_theta:
+        dm = d - half_a_theta
+        if d==0:
+            dm=0
+
+        m = int(np.ceil(np.pi * np.cos(dm) / half_a_theta))
+        ip = 2 * np.pi / m
+        dp = np.arctan(np.tan(d + half_a_theta) * np.cos(ip / 2))
+
+        ring_data[d/d2r] = []
+        for i in range(m):
+            a = i * ip
+            ring_data[d/d2r].append(a/d2r)
+
+        d = half_a_theta + dp
+        ring += 1
+
+    return ring_data
+
+def main_cport():
     d2r = np.pi / 180
     a_deg = 3.955 # cell size in degrees
 
@@ -43,5 +74,5 @@ def compare():
 
 
 if __name__=="__main__":
-    main()
+    main_cport()
     compare()
